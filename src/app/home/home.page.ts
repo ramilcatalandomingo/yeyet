e180a7661c79c581ed24dd67493915c7e7d36763
Ramil Domingo <ramildomingo@Admins-MacBook-Pro.local>
2020-11-12 21:48:13 +0800

    Implementing background tasks and local storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { Insomnia } from '@ionic-native/insomnia/ngx';
 import { NavigationBar } from '@ionic-native/navigation-bar/ngx';
 import { Plugins, LocalNotificationEnabledResult, LocalNotificationActionPerformed, LocalNotification, Device, LocalNotificationPendingList } from '@capacitor/core';
 import { AlertController } from '@ionic/angular';
-const { LocalNotifications } = Plugins;
+const { LocalNotifications, App, BackgroundTask, Storage } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -46,13 +46,40 @@ export class HomePage implements OnInit {
 
     LocalNotifications.addListener('localNotificationReceived', (notification: LocalNotification) => {
       // this.presentAlert(`Received: ${notification.title}`, `${notification.body}`);
-      console.log(`Received: ${notification.title}`, `${notification.body}`);
+      // console.log(`Received: ${notification.title}`, `${notification.body}`);
     });
 
     LocalNotifications.addListener('localNotificationActionPerformed', (notification: LocalNotificationActionPerformed) => {
       // this.presentAlert(`Performed: ${notification.actionId}`, null);
-      console.log(`Performed: ${notification.actionId}`, null);
+      // console.log(`Performed: ${notification.actionId}`, null);
     });
+
+    App.addListener('appStateChange', (state) => {
+
+      if (!state.isActive) {
+        // The app has become inactive. We should check if we have some work left to do, and, if so,
+        // execute a background task that will allow us to finish that work before the OS
+        // suspends or terminates our app:
+    
+        let taskId = BackgroundTask.beforeExit(async () => {
+          // In this function We might finish an upload, let a network request
+          // finish, persist some data, or perform some other task
+
+          Storage.set({
+            key: 'timeLeft',
+            value: JSON.stringify(this.timeLeft)
+          });
+
+          // Must call in order to end our task otherwise
+          // we risk our app being terminated, and possibly
+          // being labeled as impacting battery life
+
+          BackgroundTask.finish({
+            taskId
+          });
+        });
+      } 
+    })
   }
 
   async presentAlert(header, message) {
